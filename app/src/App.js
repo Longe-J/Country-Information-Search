c3a59b57ce117c06977e27a4a4106333958653bd
Longe-J <161849012+Longe-J@users.noreply.github.com>
2024-03-03 13:55:31 +0000

    Country Search app complete for the most part
    The app is complete when you search for a country it shows you the information about the country but it also shows you an image however improvements can be made. Sometimes the image doesn't properly fit on the screen and there is also no error handling done for when the user searches for an invalid country. And there are no animations or moving parts.

--- app/src/App.js
+++ app/src/App.js
@@ -4,30 +4,42 @@ import { useState } from "react";
 function App() {
 
   const [background, SetBack] = useState();
+  const [searchV, SetSearch] = useState();
+  const [info, setInfo] = useState([]);
   
-  const getImage =()=>{
-    const value = fetch('https://api.unsplash.com/photos/random?client_id=fff5lYbBRBvTESnKkX5AzMO0RnSmjPveHl736cPSSIo')
+  const getImage =(actualSearch)=>{
+    const value = fetch(`https://api.unsplash.com/photos/random?client_id=fff5lYbBRBvTESnKkX5AzMO0RnSmjPveHl736cPSSIo&query=${actualSearch}`)
     .then(result => {
       return result.json()
     })
     .then(data => {
-      SetBack(data.urls.full);
+      SetBack(data.urls.raw);
     })
-    
-    
-    console.log(background)
+
+    console.log(background);
+  }
+
+  const getLocation = (country)=>{
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(res => {return res.json()})
+    .then(data => setInfo(data))
   }
 
   
   
   
   return (
-    <div className="App" style={{backgroundImage: `url(${background})`}}>
+    <div className="App" style={{backgroundImage: `url(${background})`, backgroundSize: "cover"}}>
       <div className="card">
         <div className="search">
-          <input type="text" placeholder="Enter The Country You Want Info On"/>
-          <button className="btn"onClick={getImage}>Search</button>
+          <input type="text" placeholder="Enter The Country You Want Info On" onChange={event => SetSearch(event.target.value) }/>
+          <button className="btn"onClick={()=>{getImage(searchV); getLocation(searchV)}}>Search</button>
         </div>
+        {info[0] ? <h1 className="data">{info[0].name.common}</h1> : null}
+        {info[0] ? <h2 className="data">{info[0].capital[0]}</h2>: null}
+        {info[0] ? <h3 className="data">{info[0].continents[0]}</h3>: null}
+        {info[0] ? <h3 className="data">Population: {info[0].population}</h3>: null}
+        {info[0] ? <img src={info[0].flags.png} alt="" />: null}
       </div>
     </div>
   );
